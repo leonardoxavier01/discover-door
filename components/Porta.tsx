492fcd7438e18da404301898b9b3c787b7bfad84
unknown <leonardoxavier092@gmail.com>
2022-06-07 23:10:40 -0300

    chore: install typescript

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,13 +1,19 @@
+import Door from "../model/door";
 import styles from "../styles/Porta.module.css";
 
-const Porta = (props) => {
-  const selected = props.selected ? styles.selected : "";
+interface PortaProps {
+  porta: Door;
+}
+
+const Porta = (props: PortaProps) => {
+  const { porta } = props;
+  const selected = porta.selected ? styles.selected : "";
 
   return (
     <div className={styles.area}>
       <div className={`${styles.structure} ${selected}`}>
         <div className={styles.door}>
-          <div className={styles.number}>3</div>
+          <div className={styles.number}>{porta.number}</div>
           <div className={styles.doorhandle}></div>
         </div>
       </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Porta from "../components/Porta";
 import Presente from "../components/Presente";
+import Door from "../model/door";
 
 export default function Home() {
+  const [p1, setP1] = useState(new Door(1));
+
   return (
     <div style={{ display: "flex" }}>
-      <Porta selected={true} />
-      <Porta selected={false} />
+      <Porta porta={p1} />
     </div>
   );
 }
