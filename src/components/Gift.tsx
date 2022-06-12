969768b6f263b2ada6f772f1afcc23185591c214
unknown <leonardoxavier092@gmail.com>
2022-06-12 00:18:23 -0300

    feat: Gift component

---
+++ src/components/Gift.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Gift.module.css";
+
+const Presente = () => {
+  return (
+    <div className={styles.presente}>
+      <div className={styles.top}></div>
+      <div className={styles.body}></div>
+      <div className={styles.tie1}></div>
+      <div className={styles.tie2}></div>
+    </div>
+  );
+};
+
+export default Presente;
