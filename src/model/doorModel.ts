cbcb53d3da42c6f389acf2ddc16b40f2dfa2822e
unknown <leonardoxavier092@gmail.com>
2022-06-12 00:15:01 -0300

    feat: doorModel

---
+++ src/model/doorModel.ts
@@ -0,0 +1,48 @@
+export default class Door {
+  #number: number;
+  #haveGift: boolean;
+  #selected: boolean;
+  #open: boolean;
+
+  constructor(number: number, haveGift = false, selected = false, open = false) {
+    this.#number = number;
+    this.#haveGift = haveGift;
+    this.#selected = selected;
+    this.#open = open;
+  }
+
+  get number() {
+    return this.#number;
+  }
+
+  get haveGift() {
+    return this.#haveGift;
+  }
+
+  get selected() {
+    return this.#selected;
+  }
+
+  get open() {
+    return this.#open;
+  }
+
+  get closed() {
+    return !this.open;
+  }
+
+  deselect() {
+    const selected = false;
+    return new Door(this.number, this.haveGift, selected, this.open);
+  }
+
+  alterSelection() {
+    const selected = !this.selected;
+    return new Door(this.number, this.haveGift, selected, this.open);
+  }
+
+  willOpen() {
+    const open = true;
+    return new Door(this.number, this.haveGift, this.selected, open);
+  }
+}
